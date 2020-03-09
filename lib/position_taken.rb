5b3091bf990749330c23d6d0e89fde6e0e0f37e1
Nguyen Van Say <vansay@hotmail.fr>
2020-03-09 13:51:04 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 def position_taken?(board, index_number)
   position = board[index_number]
-  if position == " " || position == "" || position = nil
+  if position == " " || position == "" || position == nil
     false
   end
 end
