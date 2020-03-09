5cfa04557fd84e1f3671bddfa0086517f1faa066
Nguyen Van Say <vansay@hotmail.fr>
2020-03-09 13:52:08 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,5 +3,7 @@ def position_taken?(board, index_number)
   position = board[index_number]
   if position == " " || position == "" || position == nil
     false
+  elsif position == "X" || position == "O"
+    true
   end
 end
